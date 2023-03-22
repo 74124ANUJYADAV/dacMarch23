f9cc031a444f75b8e40e4a2c03085ae072cfa6fe
ANUJ YADAV <125472616+74124ANUJYADAV@users.noreply.github.com>
2023-03-22 17:20:22 +0530

    Add files via upload

---
+++ pattern14.java
@@ -0,0 +1,14 @@
+class pattern14{
+	public static void main(String args[]){
+		for(int i=5;i>=1;i--)
+		{
+			
+			for(int j=0;j<=i-1;j++)
+			{
+				System.out.print((j+1)+" ");
+			}
+			System.out.println();
+			
+		}
+	}
+}
---
+++ pattern8.java
@@ -0,0 +1,19 @@
+class pattern8{
+	public static void main(String args[]){
+		
+		for(int i=1;i<=5;i++)
+		{
+			for(int k=4;k>=i;k--)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=5;j++)
+			{
+				if(j>5-i)
+				System.out.print(j+" ");
+			}
+			System.out.println();
+			
+		}
+	}
+}
